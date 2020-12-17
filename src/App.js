ca6c3596847a38202b76182c1d80ebd07590ee98
Abdul-Razak <arazak4r@gmail.com>
2020-12-17 21:32:59 +0100

    special fonts added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 
 import "./App.scss";
 import store from "./redux/store";
+import { Navbar } from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="app">
+        <Navbar />
         <h1>Setup React Redux</h1>
       </div>
     </Provider>
