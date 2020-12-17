95223cc7998357a1722d55cbdf36856886473a19
Abdul-Razak <arazak4r@gmail.com>
2020-12-17 23:17:48 +0100

    main component added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Provider } from "react-redux";
 import "./App.scss";
 import store from "./redux/store";
 import { Navbar } from "./components/navbar/Navbar";
-
+import Main from "./components/main/Main";
 const App = () => {
   return (
     <Provider store={store}>
       <div className="app">
         <Navbar />
+        <Main />
         <h1>Setup React Redux</h1>
       </div>
     </Provider>
