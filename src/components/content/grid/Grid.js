d0149dc009e3d4ece2cca4fbeeeb7066dba70dc4
Abdul-Razak <arazak4r@gmail.com>
2020-12-21 22:18:36 +0100

    grid component updated to include ratings for each movies

--- src/components/content/grid/Grid.js
+++ src/components/content/grid/Grid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rating from "../rating/Rating";
 
 import "./Grid.scss";
 
@@ -14,10 +15,15 @@ const Grid = (props) => {
               style={{ backgroundImage: `url(${image.url})` }}
             >
               <div className="grid-read-more">
-                <button className="grid-cell-button">Details</button>
+                <button className="grid-cell-button">Read More</button>
               </div>
               <div className="grid-detail">
-                <span className="grid-detail-title">Pam</span>
+                <span className="grid-detail-title">Mission Impossible</span>
+                <div className="grid-detail-rating">
+                  <Rating rating={image.rating} totalStars={10} />
+                  &nbsp;&nbsp;
+                  <div className="grid-vote-average">{image.rating}</div>
+                </div>
               </div>
             </div>
           </div>
